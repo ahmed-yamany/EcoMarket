2d8fc69cfa34f5a99c8893ef96f20e5b23f40cab
Ahmed Mohammed Yamany <yamany2862016@gmail.com>
2024-01-17 10:38:48 +0200

    fix successViewControllrt constraints issues

--- EcoMarket/SceneDelegate.swift
+++ EcoMarket/SceneDelegate.swift
@@ -16,7 +16,7 @@ class SceneDelegate: UIResponder, UIWindowSceneDelegate {
                options connectionOptions: UIScene.ConnectionOptions) {
         guard let windowScene = (scene as? UIWindowScene) else { return }
         let window = UIWindow(windowScene: windowScene)
-        window.rootViewController = ViewController()
+        window.rootViewController = SuccessViewController()
         window.makeKeyAndVisible()
         self.window = window
     }
