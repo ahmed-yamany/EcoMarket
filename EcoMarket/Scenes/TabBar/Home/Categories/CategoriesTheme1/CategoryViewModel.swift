42a64253ad52851213b28b8b9440acdaa9f17fe8
Ibrahim Nasser <thenasserr@gmail.com>
2024-01-21 20:03:31 +0200

    updates

--- EcoMarket/Scenes/TabBar/Home/Categories/CategoriesTheme1/CategoryViewModel.swift
+++ EcoMarket/Scenes/TabBar/Home/Categories/CategoriesTheme1/CategoryViewModel.swift
@@ -23,8 +23,8 @@ class CategoryViewModel {
                 CategoryModel(image: "category/icon-2", name: "Clothes", productNumber: "200 Product"),
                 CategoryModel(image: "category/icon-3", name: "Bags", productNumber: "130 Product"),
                 CategoryModel(image: "category/icon-4", name: "Shoese", productNumber: "100 Product"),
-                CategoryModel(image: "category/icon-5", name: "Electronics", productNumber: "190 Product"),
-                CategoryModel(image: "category/icon-6", name: "Jewelry", productNumber: "80 Product")
+                CategoryModel(image: "category/icon-6", name: "Electronics", productNumber: "190 Product"),
+                CategoryModel(image: "category/icon-5", name: "Jewelry", productNumber: "80 Product")
             ]
             
             DispatchQueue.main.async {
