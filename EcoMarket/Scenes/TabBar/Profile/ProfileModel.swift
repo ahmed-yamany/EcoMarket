86e2954045c5b3362784a17b45f65abcb7505f93
Ibrahim Nasser <thenasserr@gmail.com>
2024-02-13 00:40:22 +0200

    add action in model

--- EcoMarket/Scenes/TabBar/Profile/ProfileModel.swift
+++ EcoMarket/Scenes/TabBar/Profile/ProfileModel.swift
@@ -10,6 +10,7 @@ import Foundation
 struct ProfileModel {
     let title: String
     let image: String
+    let action: () -> Void
 }
 
 struct UserModel {
@@ -21,30 +22,58 @@ struct UserModel {
 
 extension ProfileModel {
     static let mockData1 = [
-        ProfileModel(title: "Personal Details", image: "profile/icon"),
-        ProfileModel(title: "My Order", image: "profile/icon-2"),
-        ProfileModel(title: "My Favourites", image: "profile/icon-3"),
-        ProfileModel(title: "Shipping Address", image: "profile/icon-4"),
-        ProfileModel(title: "My Card", image: "profile/icon-5"),
-        ProfileModel(title: "Settings", image: "profile/icon-6")
+        ProfileModel(title: "Personal Details", image: "profile/icon", action: {
+            print("Personal Details")
+        }),
+        ProfileModel(title: "My Order", image: "profile/icon-2", action: {
+            print("My Order")
+        }),
+        ProfileModel(title: "My Favourites", image: "profile/icon-3", action: {
+            print("My Favourites")
+        }),
+        ProfileModel(title: "Shipping Address", image: "profile/icon-4", action: {
+            print("Shipping Address")
+        }),
+        ProfileModel(title: "My Card", image: "profile/icon-5", action: {
+            print("My Card")
+        }),
+        ProfileModel(title: "Settings", image: "profile/icon-6", action: {
+            print("Settings")
+        })
     ]
     
     static let mockData2 = [
-        ProfileModel(title: "FAQs", image: "profile/icon-7"),
-        ProfileModel(title: "Privacy Policy", image: "profile/icon-8"),
-        ProfileModel(title: "Community", image: "profile/icon-9")
+        ProfileModel(title: "FAQs", image: "profile/icon-7", action: {
+            
+        }),
+        ProfileModel(title: "Privacy Policy", image: "profile/icon-8", action: {
+            
+        }),
+        ProfileModel(title: "Community", image: "profile/icon-9", action: {
+            
+        })
     ]
     
     static let mockData3 = [
-        ProfileModel(title: "Language", image: "settings/icon"),
-        ProfileModel(title: "Notification", image: "settings/icon-2"),
-        ProfileModel(title: "Dark Mood", image: "settings/icon-3"),
-        ProfileModel(title: "Help Center", image: "settings/icon-4")
+        ProfileModel(title: "Language", image: "settings/icon", action: {
+            
+        }),
+        ProfileModel(title: "Notification", image: "settings/icon-2", action: {
+            
+        }),
+        ProfileModel(title: "Dark Mood", image: "settings/icon-3", action: {
+            
+        }),
+        ProfileModel(title: "Help Center", image: "settings/icon-4", action: {
+            
+        })
     ]
 }
 
 extension UserModel {
-    static let user = [UserModel(image: "profile/user", name: "Fscreation", email: "Fscreation441@gmail.com", action: {
+    static let user = [
+        UserModel(image: "profile/user", name: "Fscreation", email: "Fscreation441@gmail.com", action: {
         print("Profile")
-    })]
+    })
+    ]
 }
