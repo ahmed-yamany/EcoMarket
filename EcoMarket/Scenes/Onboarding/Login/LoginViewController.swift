fbed857cb3d41df022febe53dc9daa4f2b78af9e
Ibrahim Nasser <thenasserr@gmail.com>
2024-02-13 17:27:37 +0200

    fix login screen ui

--- EcoMarket/Scenes/Onboarding/Login/LoginViewController.swift
+++ EcoMarket/Scenes/Onboarding/Login/LoginViewController.swift
@@ -16,7 +16,11 @@ class LoginViewController: UIViewController {
     @IBOutlet weak var titleLabel: UILabel!
     @IBOutlet weak var subtitleLabel: UILabel!
     @IBOutlet weak var loginButton: PrimaryButton!
+    @IBOutlet weak var lineView: UIView!
+    @IBOutlet weak var orLabel: UILabel!
     
+    // MARK: - Life Cycle
+    //
     override func viewDidLoad() {
         super.viewDidLoad()
         configureViews()
@@ -24,24 +28,37 @@ class LoginViewController: UIViewController {
     
     // MARK: - Setup UI
     private func configureViews() {
-        // Email TextField
-        emailTextField.title = L10n.email
-        emailTextField.placeholder = L10n.Login.Email.placeholder
-        
-        // Password TextField UI
-        passwordTextField.title = L10n.password
-        passwordTextField.placeholder = L10n.Login.Password.placeholder
+        view.backgroundColor = AppColor.backgroundColor
+        lineView.backgroundColor = AppColor.textFieldUnderLine
         
         // Images UI
         logoImage.image = AppImage.appLogo
         
-        // Labels UI
-        titleLabel.text = L10n.Login.title
-        titleLabel.font = .h1
-        subtitleLabel.text = L10n.Login.subtitle
-        subtitleLabel.font = .medium
-        
         // Buttons UI
         loginButton.title = L10n.login
+        
+        configureEmailTextField()
+        configurePasswordTextField()
+        configureLabelsUI()
+    }
+    
+    private func configureEmailTextField() {
+        emailTextField.title = L10n.email
+        emailTextField.placeholder = L10n.Login.Email.placeholder
+    }
+    
+    private func configurePasswordTextField() {
+        passwordTextField.title = L10n.password
+        passwordTextField.placeholder = L10n.Login.Password.placeholder
+    }
+    
+    private func configureLabelsUI() {
+        titleLabel.text = L10n.Login.title
+        titleLabel.textColor = AppColor.primaryText
+        titleLabel.font = .h1
+        subtitleLabel.text = L10n.Login.subtitle
+        subtitleLabel.textColor = AppColor.socialButton
+        subtitleLabel.font = .medium
+        orLabel.textColor = AppColor.primaryText
     }
 }
