4e4548837fdf724283b02a0b658d2a3cb6141224
Ahmed Mohammed Yamany <yamany2862016@gmail.com>
2024-02-14 21:25:42 +0200

    change collection section header folder

--- EcoMarket/Scenes/TabBar/Cart/CartTheme1/CartViewController.swift
+++ EcoMarket/Scenes/TabBar/Cart/CartTheme1/CartViewController.swift
@@ -80,40 +80,5 @@ class CartViewController: UICollectionViewController {
         sections[indexPath.section].collectionView(collectionView, viewForSupplementaryElementOfKind: kind, at: indexPath)
     }
     override func collectionView(_ collectionView: UICollectionView, didSelectItemAt indexPath: IndexPath) {
-        coordinator.showLogin()
-    }
-}
-    
-extension UICollectionReusableView: Identifiable {}
-
-class Header: UICollectionReusableView {
-    static let elementKind = String("My Cart")
-    
-    let label = UILabel()
-    
-    // MARK: - Initialization
-    //
-    override init(frame: CGRect) {
-        super.init(frame: frame)
-        setupLabel()
-    }
-    
-    override func layoutSubviews() {
-        super.layoutSubviews()
-        label.frame = bounds
-    }
-    
-    required init?(coder: NSCoder) {
-        fatalError("init(coder:) has not been implemented")
-    }
-    
-    private func setupLabel() {
-        label.font = .h2
-        label.textColor = AppColor.primaryText
-        addSubview(label)
-    }
-    
-    public func setTitle(_ title: String) {
-        label.text = title
     }
 }
