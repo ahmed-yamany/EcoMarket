2ed36c81edae43a2f3429e82af28a83625b63c56
Ibrahim Nasser <thenasserr@gmail.com>
2024-03-11 16:14:08 +0200

    adding collection view and wishlist section

---
+++ EcoMarket/Scenes/WishList/Section/WishListCollectionViewCell.swift
@@ -0,0 +1,69 @@
+//
+//  WishListCollectionViewCell.swift
+//  EcoMarket
+//
+//  Created by Ibrahim Nasser Ibrahim on 11/03/2024.
+//
+
+import UIKit
+
+class WishListCollectionViewCell: UICollectionViewCell {
+
+    @IBOutlet weak var containerView: UIStackView!
+    @IBOutlet weak var addToCartButton: PrimaryButton!
+    @IBOutlet weak var productPrice: UILabel!
+    @IBOutlet weak var productName: UILabel!
+    @IBOutlet weak var brandName: UILabel!
+    @IBOutlet weak var productImage: UIImageView!
+    
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        configureUI()
+    }
+
+    // MARK: - Data Setup Method
+    func setup(product: Product) {
+        productName.text = product.name
+        brandName.text = product.brandName
+        productPrice.text = String(product.price)
+        productImage.image = product.image
+    }
+    
+    // MARK: - UI Configuration
+    private func configureUI() {
+        clipsToBounds = false
+        backgroundColor = .clear
+        productImage.layer.cornerRadius = 8
+        configureLabelsUI()
+        configureContainerView()
+    }
+    
+    private func configureLabelsUI() {
+        // Labels Font
+        brandName.font = .medium
+        productPrice.font = .medium
+        productName.font = .regular
+        
+        // Labels Color
+        brandName.textColor = AppColor.primaryText
+        productPrice.textColor = AppColor.primaryText
+        productName.textColor = AppColor.socialButton
+    }
+    
+    private func configureContainerView() {
+        containerView.layoutMargins = .init(top: 10, left: 10, bottom: 10, right: 10)
+        containerView.isLayoutMarginsRelativeArrangement = true
+        
+        // Adding a custom view to the container with shadow
+        containerView.backgroundColor = AppColor.mainTheme
+        containerView.layer.shadowColor = AppColor.primaryButton.cgColor
+        containerView.layer.shadowOffset = .zero
+        containerView.layer.shadowOpacity = 0.2
+        containerView.layer.shadowRadius = 5
+        containerView.layer.cornerRadius = 20
+    }
+    
+    private func configureAddToCartButtonUI() {
+        addToCartButton.title = "Add to cart"
+    }
+}
--- EcoMarket/Scenes/WishList/WishListViewController.swift
+++ EcoMarket/Scenes/WishList/WishListViewController.swift
@@ -9,21 +9,71 @@ import UIKit
 
 class WishListViewController: UIViewController {
 
+    // MARK: - Properties
+    var sections: [any SectionsLayout] = []
+    
+    @IBOutlet weak var stackView: UIStackView!
+    @IBOutlet weak var collectionView: UICollectionView!
+    @IBOutlet weak var scanButton: UIButton!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-
-        // Do any additional setup after loading the view.
+        
     }
-
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
+    
+    // MARK: - UI Configuration
+    private func configureUI() {
+        view.backgroundColor = AppColor.backgroundColor
+        stackView.layoutMargins = UIEdgeInsets(top: 0, left: 25, bottom: 0, right: 25)
+        stackView.isLayoutMarginsRelativeArrangement = true
+        setupButtonsUI()
     }
-    */
+    
+    private func setupButtonsUI() {
+        scanButton.setTitle("", for: .normal)
+        scanButton.setImage(AppImage.HomeTheme2.filterButtonIcon, for: .normal)
+    }
+    
+    private func configureCollectionView() {
+        collectionView.delegate = self
+        collectionView.dataSource = self
+        sections.forEach { section in
+            section.registerCell(in: self.collectionView)
+            section.registerSupplementaryView(in: self.collectionView)
+        }
+        collectionView.backgroundColor = AppColor.backgroundColor
+        collectionView.collectionViewLayout = createCompositionalLayout()
+    }
+    
+    // MARK: - Compositional Layout
+    private func createCompositionalLayout() -> UICollectionViewCompositionalLayout {
+        return UICollectionViewCompositionalLayout { (sectionIndex, layoutEnvironment) in
+            self.sections[sectionIndex].sectionLayout(self.collectionView, layoutEnvironment: layoutEnvironment)
+        }
+    }
+}
 
+extension WishListViewController: UICollectionViewDelegate, UICollectionViewDataSource {
+    // MARK: - UICollectionViewDataSource
+    func numberOfSections(in collectionView: UICollectionView) -> Int {
+        return sections.count
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        sections[section].numberOfItems()
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        sections[indexPath.section].collectionView(collectionView, cellForItemAt: indexPath)
+    }
+    
+    func collectionView(_ collectionView: UICollectionView,
+                        viewForSupplementaryElementOfKind kind: String,
+                        at indexPath: IndexPath) -> UICollectionReusableView {
+        sections[indexPath.section].collectionView(collectionView, viewForSupplementaryElementOfKind: kind, at: indexPath)
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, didSelectItemAt indexPath: IndexPath) {
+        sections[indexPath.section].collectionView(collectionView, didSelectItemAt: indexPath)
+    }
 }
