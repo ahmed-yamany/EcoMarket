ca881ab24c6cacbcc27dc0808776640597c85fa8
Ibrahim Nasser <thenasserr@gmail.com>
2024-03-11 15:39:03 +0200

    adding wish list view controller

---
+++ EcoMarket/Scenes/WishList/WishListViewController.swift
@@ -0,0 +1,29 @@
+//
+//  WishListViewController.swift
+//  EcoMarket
+//
+//  Created by Ibrahim Nasser Ibrahim on 11/03/2024.
+//
+
+import UIKit
+
+class WishListViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
